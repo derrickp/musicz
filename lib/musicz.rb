7807d250905c23d63a193578f9863443a9e02496
Derrick Plotsky <derrickplotsky@gmail.com>
2019-11-17 22:10:05 -0600

    Initial commit

---
+++ lib/musicz.rb
@@ -0,0 +1,6 @@
+require "musicz/version"
+
+module Musicz
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/musicz_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Musicz do
+  it "has a version number" do
+    expect(Musicz::VERSION).not_to be nil
+  end
+end
