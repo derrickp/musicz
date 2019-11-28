1fbac46f904bd2ed341da0a6cea0967f0975668d
Derrick Plotsky <derrickplotsky@gmail.com>
2019-11-27 21:36:06 -0800

    Update entity attributes with defaults
    Both of these actually came back as null from the API, which didn't
    happen when I was looking at the original query.
    
    I assume this is going to happen a lot, so I'm prepping for a bunch of
    entity updates.

--- lib/musicz/entities/alias.rb
+++ lib/musicz/entities/alias.rb
@@ -8,7 +8,7 @@ module Musicz
       attribute :name, Types::Strict::String
       attribute :sort_name, Types::Strict::String
       attribute? :locale, Types::Strict::String.optional
-      attribute? :primary, Types::Strict::Bool
+      attribute :primary, Types::Strict::Bool.default(false)
     end
   end
 end
--- lib/musicz/entities/life_span.rb
+++ lib/musicz/entities/life_span.rb
@@ -5,7 +5,7 @@ require 'musicz/entity'
 module Musicz
   module Entities
     class LifeSpan < Entity
-      attribute :ended, Types::Strict::Bool
+      attribute :ended, Types::Strict::Bool.default(false)
       attribute? :begin, Types::Strict::String.optional
       attribute? :end, Types::Strict::String.optional
     end
