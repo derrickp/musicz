04806347906d7213c063a5dd7a73f8bc81977047
Derrick Plotsky <derrickplotsky@gmail.com>
2019-12-02 20:52:14 -0800

    Add ArtistQueryTerms
    These are going to be the terms that you could use to search for a list
    of artists from the musicbrainz search API.
    
    To use this, at least one of the terms needs to be filled in, but only
    one is necessary.

---
+++ lib/musicz/search/options/artist_query_terms.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'musicz/entity'
+
+module Musicz
+  module Search
+    module Options
+      # Setting any of these and passing into the repo terms query will
+      # include them in the query to the endpoint
+      class ArtistQueryTerms < Entity
+        attribute? :alias, Types::Strict::String.optional
+        attribute? :area, Types::Strict::String.optional
+        attribute? :artist, Types::Strict::String.optional
+        attribute? :artistaccent, Types::Strict::String.optional
+        attribute? :begin, Types::Strict::String.optional
+        attribute? :beginarea, Types::Strict::String.optional
+        attribute? :comment, Types::Strict::String.optional
+        attribute? :country, Types::Strict::String.optional
+        attribute? :end, Types::Strict::String.optional
+        attribute? :endarea, Types::Strict::String.optional
+        attribute? :gender, Types::Strict::String.optional
+        attribute? :ipi, Types::Strict::String.optional
+        attribute? :sortname, Types::Strict::String.optional
+        attribute? :tag, Types::Strict::String.optional
+        attribute? :type, Types::Strict::String.optional
+      end
+    end
+  end
+end
