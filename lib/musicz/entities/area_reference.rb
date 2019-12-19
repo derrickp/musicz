80f2d0f06b54b3a46922fe199916572f208adb21
Derrick Plotsky <derrickplotsky@gmail.com>
2019-12-19 11:38:33 -0800

    Update Area entities
    This is so that our Area can contain the other areas it is related to.
    For instance a city will be related to the greater region such as
    province or regional district it belongs to.

--- lib/musicz/entities/area_reference.rb
+++ lib/musicz/entities/area_reference.rb
@@ -1,17 +1,13 @@
 # frozen_string_literal: true
 
 require 'musicz/entity'
-require 'musicz/entities/area_relation'
 
 module Musicz
   module Entities
-    class Area < Entity
+    class AreaReference < Entity
       attribute :id, Types::Strict::String
       attribute :name, Types::Strict::String
       attribute :sort_name, Types::Strict::String
-      attribute? :relations, Types::Array.of(AreaRelation).default([].freeze)
-      attribute :iso_3166_1_codes, Types::Array.of(Types::Strict::String).
-        default([].freeze)
       attribute? :disambiguation, Types::Strict::String.optional
     end
   end
---
+++ lib/musicz/entities/area_relation.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'musicz/entity'
+require 'musicz/entities/area_reference'
+
+module Musicz
+  module Entities
+    class AreaRelation < Entity
+      attribute? :attribute_ids, Types::Strict::Hash.optional
+      attribute :direction, Types::Strict::String
+      attribute :type_id, Types::Strict::String
+      attribute :ended, Types::Strict::Bool.default(false)
+      attribute? :end, Types::Strict::String.optional
+      attribute :area, AreaReference
+    end
+  end
+end
