e7f68b12b9239f381f18109092cf82ebc26ee355
Derrick Plotsky <derrickplotsky@gmail.com>
2020-01-12 14:09:04 -0800

    Add releases to Recording
    With a specific recording, the releases that they had are super useful.
    This helps you get from the song to the actual album that it was in.

--- lib/musicz/entities/recording.rb
+++ lib/musicz/entities/recording.rb
@@ -2,6 +2,7 @@
 
 require 'musicz/entity'
 require 'musicz/entities/artist_credit'
+require 'musicz/entities/release_reference'
 
 module Musicz
   module Entities
@@ -9,9 +10,11 @@ module Musicz
       attribute :id, Types::Strict::String
       attribute :title, Types::Strict::String
       attribute? :disambiguation, Types::Strict::String.optional
+      attribute? :score, Types::Strict::Integer.optional
       attribute? :artist_credit, Types::Array.of(ArtistCredit)
       attribute? :length, Types::JSON::Decimal.optional
-      attribute? :isrcs, Types::Array.of(Types::Strict::String)
+      attribute? :isrcs, Types::Array.of(Types::Strict::String).default([].freeze)
+      attribute? :releases, Types::Array.of(ReleaseReference)
     end
   end
 end
