aff72a01717ba42177144d002b5d106f9e577487
Derrick Plotsky <derrickplotsky@gmail.com>
2020-01-12 14:15:45 -0800

    Add ReleaseGroup entity
    I think this keeps track of groups of recordings/releases, so something
    like an Album.

---
+++ lib/musicz/entities/release_group.rb
@@ -0,0 +1,12 @@
+module 'musicz/entity'
+
+module Musicz
+  module Entities
+    class ReleaseGroup < Entity
+      attribute :id, Types::Strict::String
+      attribute :type_id, Types::Strict::String
+      attribute? :title, Types::Strict::String.optional
+      attribute? :primary_type, Types::Strict::String.optional
+    end
+  end
+end
